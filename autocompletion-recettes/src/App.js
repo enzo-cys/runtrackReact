06f4f122ec9b96c9c64381e81e24e93ef0414fa1
enzo-cys <enzo.cys@laplateforme.io>
2026-01-20 14:59:24 +0100

    Job 00 - Configuration et découverte de l'API

---
+++ autocompletion-recettes/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenue dans l'application d'autocomplétion de recettes !</h1>
+    </div>
+  );
+}
+
+export default App;
